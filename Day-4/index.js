d341c168997840ce991cd23984aacce5a5906c2d
Shivanisunil281109 <shivaniesonawane@gmail.com>
2026-06-14 01:10:26 +0530

    Updates for Day-4

---
+++ Day-4/index.js
@@ -0,0 +1,64 @@
+// Array
+
+   var students =[   
+       
+       
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul", 
+       "Virat","Rohit","Rahul",
+        "Awdiz",
+       
+   
+       
+   
+     
+    
+   
+ ];
+
+  console.log(students );
+  console.log(students [1]);
+ console.log(students [2]);
+  console.log(students [3]);
+  console.log(students.length);
+    console.log(students[students.length-1]);
+
+console.log((students.length-1) / 2);
+console.log(students[(students.length-1) / 2]);
+students[(students.length-1)/2]=  "Awdiz";
+console.log(students[(students.length-1)/2])  
+
+students[90]= "Hello";
+console.log(students[90]);
+console.log(students);
+
+
+students[90]= "Hii";
+console.log(students[90]);
+console.log(students);
+console.log(students[89]);
